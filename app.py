d70fb68ec3499271299b94f03a097c98ade6b9cc
jraikhola <jraikhola@gmail.com>
2021-12-15 11:10:28 +0545

    daraz scraping app updated with new template

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from requests.utils import requote_uri
 import json
 import ast 
 from flask import jsonify
-from flask import Flask,request,render_template,redirect
+from flask import Flask,request,render_template
+
+
 
 from flask_app.methods import daraz_scraper
 
@@ -15,8 +17,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def test_base_api():
-    return render_template("index.html")
+@app.route('/index')
+def index():
+  
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=True,host= '0.0.0.0', port='5001')
+    app.run(debug=True,host= '0.0.0.0', port='5002')
